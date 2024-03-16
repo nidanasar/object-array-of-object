ea3cc954792cd9591129162c1ec1b49420e294af
nidanasar <159262333+nidanasar@users.noreply.github.com>
2024-03-17 03:56:12 +0500

    Add files via upload

---
+++ home work/home.js
@@ -0,0 +1,10 @@
+// Array 
+var countryNames = ['PAKISTAN', 'INDIA', 'JAPAN', 'CHINA'];
+var countryCodes = [92, 91, 81, 86];
+var muslimCountries = [true, false, false, false];
+var countryLeadres = [];
+console.log(countryNames);
+console.log(countryCodes);
+console.log(muslimCountries);
+console.log(countryLeadres);
+console.log('our country is', countryNames[0]);
---
+++ home work/home.ts
@@ -0,0 +1,12 @@
+// Array 
+let countryNames:string[]=['PAKISTAN','INDIA','JAPAN','CHINA'];
+let countryCodes:number[]=[92,91,81,86];
+let muslimCountries:boolean[]=[true, false, false,false];
+let countryLeadres:null[]=[];
+
+console.log(countryNames);
+console.log(countryCodes);
+console.log(muslimCountries);
+console.log(countryLeadres);
+
+console.log('our country is',countryNames[0]);
